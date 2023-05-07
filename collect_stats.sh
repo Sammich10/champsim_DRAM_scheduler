9058392baf0c7ca692e32c45baaa4c71f8b7309d
Sammich10 <sammymichelsen@gmail.com>
2023-05-06 19:31:59 -0500

    updated tace file download script

--- collect_stats.sh
+++ collect_stats.sh
@@ -6,6 +6,5 @@ for file in sim_results/*; do
     # Loop through cores from 0 to 24
     for ((i=0; i<=24; i++)); do
         grep -o "Core_${i}_IPC [0-9]*\.[0-9]*" "$file" >> stats/${filename}_stats.txt
-        grep -o "Core_${i}_branch_MPKI [0-9]*\.[0-9]*" "$file" >> stats/${filename}_stats.txt
     done
 done
